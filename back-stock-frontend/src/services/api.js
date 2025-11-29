8fd60e0a8e3a8632c548ea0b8915e681063a7b6e
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2025-11-29 00:36:12 -0300

    Update api.js

--- back-stock-frontend/src/services/api.js
+++ back-stock-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://back-stock-dagv.onrender.com/api',
+  baseURL: 'https://back-stock-dagv.onrender.com/api',
   withCredentials: true,
 });
 

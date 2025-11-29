2ff5c32a0996e762ec8443c4d337fb68b8a2b2ec
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2025-11-28 23:49:45 -0300

    Update NavBar.jsx

--- back-stock-frontend/src/components/NavBar.jsx
+++ back-stock-frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import "../assets/styles/navBar.css";
+import "../assets/styles/NavBar.css";
 import { logoutRequest } from "../services/api";
 
 const Navbar = () => {

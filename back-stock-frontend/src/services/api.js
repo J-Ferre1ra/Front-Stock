f9d92f8e42ea0445cf5e3761e6c1b4cee7d50613
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2026-03-02 21:22:51 -0300

    resolve problems URL

--- back-stock-frontend/src/services/api.js
+++ back-stock-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://back-stock-dagv.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
